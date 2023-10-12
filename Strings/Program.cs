970edd8dac7c568de7138cf4d1b5e0234f10f302
sbzapata <sbzapata@sigmaingenieria.net>
2023-10-12 12:14:20 -0500

    .Net ejercicio 5 Strings

---
+++ Strings/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Ricardo perez ID");
+int altura = 168;
+int edad = 89;
+string  nombre = "ricardo perez";
+string informacion = "Nacio en Hidalgo, es estudiante de gastronomia y astronomia ";
+var hobby = "deportista";
+
+string tarjetaIdentificacion = $"La informacion de {nombre} es la siguiente: \n"+
+    $"{informacion}, su edad es {edad} años, su altura es {altura}, su pasatiempo es {hobby}";
+
+Console.WriteLine(tarjetaIdentificacion);
+
